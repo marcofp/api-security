6757bbac615596e9d3e44c15752bfb4a6c12ed48
Marco Fuentelsaz <mfuentelsaz@fintonic.com>
2021-09-19 10:54:11 +0200

    First implementation

---
+++ src/main/java/org/marcofp/apisecurity/Main.java
@@ -0,0 +1,82 @@
+package org.marcofp.apisecurity;
+
+import java.nio.file.*;
+
+import org.marcofp.apisecurity.controller.SpaceController;
+import org.dalesbred.*;
+import org.dalesbred.result.EmptyResultException;
+import org.h2.jdbcx.*;
+import org.json.*;
+import spark.Request;
+import spark.Response;
+import spark.Spark;
+
+import static spark.Spark.*;
+
+
+public class Main {
+
+    public static void main(String... args) throws Exception {
+        var datasource = JdbcConnectionPool.create(
+                "jdbc:h2:mem:natter", "natter", "password");
+        var database = Database.forDataSource(datasource);
+        createTables(database);
+
+        JdbcConnectionPool.create(
+                "jdbc:h2:mem:natter", "natter_api_user", "password");
+        database = Database.forDataSource(datasource);
+
+
+        var spaceController = new SpaceController(database);
+        Spark.post("/spaces",
+                spaceController::createSpace);
+
+        before(((request, response) -> {
+            if (request.requestMethod().equals("POST") && !"application/json".equals(request.contentType())) {
+                halt(415, new JSONObject().put("error", "Only application/json supported").toString());
+            }
+
+            }));
+
+        after((request, response) -> {
+            response.type("application/json");
+        });
+
+        afterAfter((request, response) -> {
+            response.type("application/json;charset=utf-8");
+            response.header("X-Content-Type-Options", "nosniff");
+            response.header("X-Frame-Options", "DENY");
+            response.header("X-XSS-Protection", "0");
+            response.header("Cache-Control", "no-store");
+            response.header("Content-Security-Policy",
+                    "default-src 'none'; frame-ancestors 'none'; sandbox");
+            response.header("Server", "");
+        });
+
+        internalServerError(new JSONObject()
+                .put("error", "internal server error").toString());
+        notFound(new JSONObject()
+                .put("error", "not found").toString());
+        exception(IllegalArgumentException.class, Main::badRequest);
+        exception(JSONException.class, Main::badRequest);
+        exception(EmptyResultException.class,
+                (e, request, response) -> response.status(404));
+
+    }
+
+    private static void createTables(Database database)
+            throws Exception {
+
+        var path = Paths.get(
+                Main.class.getResource("/schema.sql").toURI());
+        database.update(Files.readString(path));
+    }
+
+    private static void badRequest(Exception ex, Request request, Response response) {
+        response.status(400);
+        response.body(new JSONObject()
+                .put("error", ex.getMessage()).toString());
+    }
+
+
+}
---
+++ src/main/java/org/marcofp/apisecurity/controller/SpaceController.java
@@ -0,0 +1,45 @@
+package org.marcofp.apisecurity.controller;
+
+import org.dalesbred.Database;
+import org.json.*;
+import spark.*;
+
+import java.sql.SQLException;
+
+
+public class SpaceController {
+
+    private final Database database;
+
+    public SpaceController(Database database) {
+        this.database = database;
+    }
+
+    public JSONObject createSpace(Request request, Response response) throws SQLException {
+        var json = new JSONObject(request.body());
+        var spaceName = json.getString("name");
+        if (spaceName.length() > 255) {
+            throw new IllegalArgumentException("space name too long");
+        }
+
+        var owner = json.getString("owner");
+        if (!owner.matches("[a-zA-Z][a-zA-Z0-9]{1,29}")) {
+            throw new IllegalArgumentException("invalid username");
+        }
+
+        return database.withTransaction(tx -> {
+            var spaceId = database.findUniqueLong(
+                    "SELECT NEXT VALUE FOR space_id_seq;");
+            database.updateUnique(
+                    "INSERT INTO spaces(space_id, name, owner) " +
+                            "VALUES(?, ?, ?);", spaceId, spaceName, owner);
+
+            response.status(201);
+            response.header("Location", "/spaces/" + spaceId);
+            return new JSONObject()
+                    .put("name", spaceName)
+                    .put("uri", "/spaces/" + spaceId);
+        });
+    }
+
+}
